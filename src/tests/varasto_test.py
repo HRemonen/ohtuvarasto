5d1094d5f08d14081f651e9b85b88436b667ce70
Henri Remonen <henri@remonen.fi>
2022-10-31 10:42:33 +0200

    Osa 1 tehtävä 8 100% haarautuvuus

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -17,7 +17,7 @@ class TestVarasto(unittest.TestCase):
         self.varasto.lisaa_varastoon(8)
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
-
+    
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -38,3 +38,46 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        
+    def test_vaara_tilavuus_annettu(self):
+        self.bad_varasto = Varasto(-2)
+
+        self.assertEqual(self.bad_varasto.tilavuus, 0.0)
+
+    def test_vaara_saldo_annettu(self):
+        self.bad_varasto = Varasto(10, -10)
+
+        self.assertEqual(self.bad_varasto.tilavuus, 10)
+        self.assertEqual(self.bad_varasto.saldo, 0)
+
+    def test_lisaa_varastoon_alle_nolla(self):
+        saldo_ennen = self.varasto.saldo
+
+        self.varasto.lisaa_varastoon(-2)
+
+        self.assertEqual(self.varasto.saldo, saldo_ennen)
+
+    def test_listaa_vastoon_liikaa(self):
+        saldo_ennen = self.varasto.saldo
+        maks = self.varasto.paljonko_mahtuu()
+
+        self.varasto.lisaa_varastoon(maks + 10)
+
+        self.assertEqual(self.varasto.saldo, (saldo_ennen + maks))
+
+    def test_ota_varastosta_alle_nolla(self):
+        self.assertEqual(self.varasto.ota_varastosta(-2), 0.0)
+
+    def test_ota_varastosta_liikaa(self):
+        maks = self.varasto.saldo
+
+        self.assertEqual(self.varasto.ota_varastosta(maks + 10), maks)
+
+        self.assertEqual(self.varasto.saldo, 0.0)
+
+    def test_print(self):
+        assert isinstance(str(self), str)
+
+        self.test_varasto = Varasto(10)
+
+        self.assertEqual(str(self.test_varasto), "saldo = 0, vielä tilaa 10")
